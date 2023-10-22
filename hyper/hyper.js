28ae49abaa8a9c5d9ee82b91b0f117e05b8348ac
Frank Davies <fdavies93@gmail.com>
2023-10-22 19:22:07 +0800

    update helix

--- hyper/hyper.js
+++ hyper/hyper.js
@@ -137,7 +137,7 @@ module.exports = {
     //   `hyperpower`
     //   `@company/project`
     //   `project#1.0.1`
-    plugins: ["native", "hyper-flat"],
+    plugins: ["hyper-monokai-pro","native", "hyper-flat"],
     // in development, you can create a directory under
     // `~/.hyper_plugins/local/` and include it here
     // to load it and avoid it being `npm install`ed
