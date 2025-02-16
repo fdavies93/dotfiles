f8c022b5f75dd5a9bed08b2158186a32f432a2ca
Frank Davies <fdavies93@gmail.com>
2025-02-16 17:23:07 +0800

    Update with cooler templates

--- nvim-kickstart/init.lua
+++ nvim-kickstart/init.lua
@@ -54,7 +54,32 @@ local plugins = {
       writing = {
         root = "~/notes/journal/2025",
         day = {
-          template = "# %A, %B %d %Y\n\n## Notes\n\n## Tasks\n\n## Timebox"
+          template = [=[
+# %A, [[%Y-%m|%B]] %d [[%Y]]
+
+[[%Y-week-%W|Week %W]]
+
+## Notes
+
+## Tasks
+
+## Timebox
+
+| Time | Task |
+|------|------|
+]=]
+        },
+        week = {
+          template = [=[
+# [[%Y-week-%W|Week %W, %Y]]
+
+Week beginning [[%Y-%m-%d]]
+]=]
+        },
+        month = {
+          template = [=[
+# %B [[%Y]]
+]=]
         }
       }
     }
