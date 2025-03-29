fc562946af7deb55acfefccc91e6f2a85f1c1af8
Frank Davies <fdavies93@gmail.com>
2025-03-29 11:26:07 +0800

    Various QoL improvements

--- wezterm/wezterm.lua
+++ wezterm/wezterm.lua
@@ -4,9 +4,11 @@ local act = wezterm.action
 return {
 	enable_tab_bar = false,
 	color_scheme = 'Oxocarbon Dark',
-	font = wezterm.font 'Hack',
+	font = wezterm.font 'Hack Nerd Font Mono',
 	enable_wayland = true,
 	keys = {
 		{ key = 'V', mods = 'CTRL', action = act.PasteFrom 'Clipboard' },
+		{ key = 'F11', action = act.ToggleFullScreen },
 	},
+	window_decorations = "RESIZE",
 }
