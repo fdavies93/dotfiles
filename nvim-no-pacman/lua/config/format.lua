11537c94521c22373c0154b05408c19ff09a5a04
Frank Davies <frank.davies@clario.com>
2025-06-20 13:22:45 +0800

    Add formatting and linting stuff

--- nvim-no-pacman/lua/config/format.lua
+++ nvim-no-pacman/lua/config/format.lua
@@ -1,6 +1,8 @@
 require("conform").setup({
   formatters_by_ft = {
     python = { "isort", "black" },
+    javascript = { "prettier", "eslint" },
+    typescript = { "prettier", "eslint" },
   },
   format_on_save = {
     -- suggested config in docs, but might need tweaking
