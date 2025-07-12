b1ec172a0b07f331ebfee08e757a192f539c4e13
Frank Davies <14893933+fdavies93@users.noreply.github.com>
2025-07-12 14:29:01 +0800

    Fix cortile script

--- scripts/cortile-move-to-monitor.sh
+++ scripts/cortile-move-to-monitor.sh
@@ -2,6 +2,8 @@
 COMMAND=$1
 
 screen_count=$(/home/frank/utils/cortile dbus -property Workplace | jq '.Data.ScreenCount')
+active_window_id=$(xdotool getwindowfocus)
+active_window_screen=""
 
 check_args(){
         if [[ -z $COMMAND || ( "$COMMAND" != 'next' && "$COMMAND" != 'prev' ) ]]; then
@@ -11,42 +13,35 @@ check_args(){
 }
 
 get_active_window_screen() {
-        local active_window_screen=$(/home/frank/utils/cortile dbus -property Clients | jq '.Data.Values.[] | select(.Window.Id == 90177539) | .Latest.Location.Screen')
-
-        echo $active_window_screen
+        active_window_screen=$(/home/frank/utils/cortile dbus -property Clients | jq --argjson ACTIVE "$active_window_id" '.Data.Values.[] | select(.Window.Id==$ACTIVE) | .Latest.Location.Screen')
 }
 
 go_next_screen () {
-        local window_id="$1"
-        local window_screen="$2"
-        local target_screen=$((window_screen+1))
+        local target_screen=$((active_window_screen+1))
         if [[ "$target_screen" -ge "$screen_count" ]]; then
                 exit 0
                 # target_screen=0 # or exit 0
         fi
-        /home/frank/utils/cortile dbus -method WindowToScreen "$window_id" "$target_screen"
+        /home/frank/utils/cortile dbus -method WindowToScreen "$active_window_id" "$target_screen"
 }
 
 
 go_prev_screen () {
-        local window_id="$1"
-        local window_screen="$2"
-        local target_screen=$((window_screen-1))
+        local target_screen=$((active_window_screen-1))
         if [[ "$target_screen" -lt 0 ]]; then
                 exit 0
                 # target_screen=$((screen_count-1)) # or exit 0
         fi
-        /home/frank/utils/cortile dbus -method WindowToScreen "$window_id" "$target_screen"
+        /home/frank/utils/cortile dbus -method WindowToScreen "$active_window_id" "$target_screen"
 }
 
 main () {
         check_args
-        local active_window_id=$(xdotool getwindowfocus)
-        local active_window_screen=$(get_active_window_screen)
+        get_active_window_screen
         if [[ "$COMMAND" == "next" ]]; then
-                go_next_screen $active_window_id $active_window_screen 
+                go_next_screen 
         else
-                go_prev_screen $active_window_id $active_window_screen
+                go_prev_screen
         fi
 }
 
